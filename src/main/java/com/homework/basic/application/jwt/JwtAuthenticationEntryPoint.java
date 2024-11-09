21106a4abcd66560dcee7697e5a4fffd2018272b
CZ-punk <cnhhw0408@gmail.com>
2024-11-09 19:18:49 +0900

    :sparkles: jwt filter, userDetails custom

---
+++ src/main/java/com/homework/basic/application/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,27 @@
+package com.homework.basic.application.jwt;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+@Component
+@Slf4j(topic = "jwt authentication entry point")
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final HandlerExceptionResolver resolver;
+
+    public JwtAuthenticationEntryPoint(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
+        this.resolver = resolver;
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) {
+        log.error("entry point start.");
+        resolver.resolveException(request, response, null, authException);
+        log.error("entry point end.");
+    }
+}
